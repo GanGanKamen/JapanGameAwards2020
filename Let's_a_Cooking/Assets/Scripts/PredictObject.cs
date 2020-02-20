e65e4b8e4e470294544750151af73375cb13ff14
GanGanKamen <m01171408c@edu.teu.ac.jp>
2020-02-20 21:14:53 +0900

    初期

---
+++ Let's_a_Cooking/Assets/Scripts/PredictObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class PredictObject : MonoBehaviour
+    {
+        //予測オブジェクトが消えるまでのカウンター
+        float desteoyCounter = 0;
+                /// <summary>
+        /// 予測線表示時間間隔です。
+        /// </summary>
+        public float predictObjInterval;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //予測オブジェクトが消えるまでのカウンターを加算する
+            //desteoyCounter += 1 * Time.deltaTime;
+
+            //4/10秒経ったとき
+            if(desteoyCounter >= 0.4f)
+            {
+                //その予測オブジェクトの消去
+                //Destroy(this.gameObject);
+            }
+        }
+    }
+
+}
