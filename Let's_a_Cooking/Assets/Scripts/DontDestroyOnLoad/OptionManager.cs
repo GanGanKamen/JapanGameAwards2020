7c22c4b13ce1f08a4fb2751b6c73c0c1e8db9d50
GanGanKamen <m01171408c@edu.teu.ac.jp>
2020-05-14 19:00:56 +0900

    仮オプション画面完成

--- Let's_a_Cooking/Assets/Scripts/DontDestroyOnLoad/OptionManager.cs
+++ Let's_a_Cooking/Assets/Scripts/DontDestroyOnLoad/OptionManager.cs
@@ -16,33 +16,62 @@ namespace Cooking
         [SerializeField] private Text seText;
         [SerializeField] private Slider seSlider;
         [SerializeField] private GameObject menuWindow;
+        [SerializeField] private Button jpnButton;
+        [SerializeField] private Button engButton;
+        [SerializeField] private Button chnButton;
 
         public UnityEngine.Audio.AudioMixer gameAudio;
 
         static public int BGM_Volume;
         static public int SE_Volume;
-
+        static public Language language;
 
         // Start is called before the first frame update
         void Start()
         {
             MenuInit();
-            UI_Init();
+            Button_Init();
+            Setdefult();
             DontDestroyOnLoad(gameObject);           
         }
 
         // Update is called once per frame
         void Update()
         {
-            BGM_Volume = (int)bgmSlider.value;
-            SE_Volume = (int)seSlider.value;
-            bgmText.text = BGM_Volume.ToString();
-            seText.text = SE_Volume.ToString();
+            SetSoundVolume(-30);
+        }
 
-            var bgmVol = BGM_Volume * 10 - 100;
-            var seVol = SE_Volume * 10 - 100;
-            gameAudio.SetFloat("BGMVol", bgmVol);
-            gameAudio.SetFloat("SEVol", seVol);
+        public void Setdefult()
+        {
+            language = GetSystemLanguage();
+            bgmSlider.value = 7;
+            seSlider.value = 7;
+            switch (language)
+            {
+                case Language.Japanese:
+                    LanguageSwitch(0);
+                    break;
+                case Language.English:
+                    LanguageSwitch(1);
+                    break;
+                case Language.Chinese:
+                    LanguageSwitch(2);
+                    break;
+            }
+        }
+
+        public Language GetSystemLanguage()
+        {
+            var osLan = Application.systemLanguage;
+            switch (osLan)
+            {
+                case SystemLanguage.Japanese:
+                    return Language.Japanese;
+                case SystemLanguage.Chinese:
+                    return Language.Chinese;
+                default:
+                    return Language.English;
+            }
         }
 
         private void MenuInit()
@@ -52,11 +81,14 @@ namespace Cooking
             menubutton.gameObject.SetActive(true);
         }
 
-        private void UI_Init()
+        private void Button_Init()
         {
             menubutton.onClick.AddListener(() => MenuOpen());
             menuWindowCloseNutton.onClick.AddListener(() => MenuClose());
             backTitleButton.onClick.AddListener(() => BackToTitle());
+            jpnButton.onClick.AddListener(() => LanguageSwitch(0));
+            engButton.onClick.AddListener(() => LanguageSwitch(1));
+            chnButton.onClick.AddListener(() => LanguageSwitch(2));
         }
 
         private void BackToTitle()
@@ -98,20 +130,59 @@ namespace Cooking
             }
             */
         }
-        /*
-        public void SetBGM(float value)
+
+        private void LanguageSwitch(int lan)
         {
-            var volume = value * 10 - 100;
-            gameAudio.SetFloat("BGMVol", volume);
-            Debug.Log(volume);
+            jpnButton.image.color = Color.white;
+            engButton.image.color = Color.white;
+            chnButton.image.color = Color.white;
+
+            switch (lan)
+            {
+                case 0:
+                    language = Language.Japanese;
+                    jpnButton.image.color = Color.yellow;
+                    break;
+                case 1:
+                    language = Language.English;
+                    engButton.image.color = Color.yellow;
+                    break;
+                case 2:
+                    language = Language.Chinese;
+                    chnButton.image.color = Color.yellow;
+                    break;
+            }
         }
 
-        public void SetSE(float value)
+        private void SetSoundVolume(int min_dB)
         {
-            var volume = value * 10 - 100;
-            gameAudio.SetFloat("SEVol", volume);
+            BGM_Volume = (int)bgmSlider.value;
+            SE_Volume = (int)seSlider.value;
+            bgmText.text = BGM_Volume.ToString();
+            seText.text = SE_Volume.ToString();
+
+            var volDelta = Mathf.Abs(0 - min_dB) / 9;
+            var bgmVol = min_dB + volDelta * BGM_Volume;
+            var seVol = min_dB + volDelta * SE_Volume;
+            if (BGM_Volume == 0)
+            {
+                gameAudio.SetFloat("BGMVol", -80);
+            }
+            else
+            {
+                gameAudio.SetFloat("BGMVol", bgmVol);
+            }
+            
+            if(SE_Volume == 0)
+            {
+                gameAudio.SetFloat("SEVol", -80);
+            }
+            else
+            {
+                gameAudio.SetFloat("SEVol", seVol);
+            }
+            
         }
-        */
     }
 
 }
