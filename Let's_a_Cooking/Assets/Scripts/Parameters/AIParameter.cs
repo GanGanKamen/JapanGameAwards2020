874bcbd6fec83ac454bdb77c78b815f79a33f23c
TakumiH0929 <takumi9292977@gmail.com>
2020-05-24 09:34:54 +0900

    <AI機能の作業中> [add]AIショットの乱数によるばらつきを追加

---
+++ Let's_a_Cooking/Assets/Scripts/Parameters/AIParameter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Cooking.Stage
+{
+    [CreateAssetMenu(fileName = "AIParameter", menuName = "ScriptableObjects/AIParameter", order = 3)]
+    public class AIParameter : ScriptableObject
+    {
+        [SerializeField, Header("Hard")]
+        private float[] _hardRandomRange = new float[System.Enum.GetValues(typeof(LimitValue)).Length];
+        public float[] HardRandomRange
+        {
+            get { return _hardRandomRange; }
+        }
+        [SerializeField, Header("Normal")]
+        private float[] _normalRandomRange = new float[System.Enum.GetValues(typeof(LimitValue)).Length];
+        public float[] NormalRandomRange
+        {
+            get { return _normalRandomRange; }
+        }
+        [SerializeField, Header("Easy")]
+        private float[] _easyRandomRange = new float[System.Enum.GetValues(typeof(LimitValue)).Length];
+        public float[] EasyRandomRange
+        {
+            get { return _easyRandomRange; }
+        }
+    }
+}
