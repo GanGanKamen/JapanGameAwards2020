2eae8e5baf43632999bbd6c801f0b69629831353
GanGanKamen <m01171408c@edu.teu.ac.jp>
2020-05-28 01:41:56 +0900

    ステージ選択画面、ステージビュー

--- Let's_a_Cooking/Assets/Scripts/SelectStage/SelectStageSceneManager.cs
+++ Let's_a_Cooking/Assets/Scripts/SelectStage/SelectStageSceneManager.cs
@@ -7,13 +7,18 @@ namespace Cooking.SelectStage
 {
     public class SelectStageSceneManager : MonoBehaviour
     {
-
-        [SerializeField] GameObject[] buttonObjects;
+        [SerializeField] private Button backButton;
+        [SerializeField] private Button goButton;
+        [SerializeField] private Button[] buttonObjects;
+        [SerializeField] private GameObject[] selectedObjects;
+        [SerializeField] private GameObject[] stageViews;
+        private int beSelectedNum = 0;
 
         // Start is called before the first frame update
         void Start()
         {
-            
+            ButtonsInit();
+            SelectStageNumber(0);
         }
 
         // Update is called once per frame
@@ -22,17 +27,39 @@ namespace Cooking.SelectStage
 
         }
         
+        private void ButtonsInit()
+        {
+            backButton.onClick.AddListener(() => OnReturnButton());
+            goButton.onClick.AddListener(() => GotoStage());
+            for(int i = 0; i< buttonObjects.Length; i++)
+            {
+                int j = i;
+                buttonObjects[j].onClick.AddListener(() => SelectStageNumber(j));
+                selectedObjects[j].SetActive(false);
+                stageViews[i].SetActive(false);
+            }
+        }
+
         public void SelectStageNumber(int stageNumber)
+        {
+            beSelectedNum = stageNumber;
+            for (int i = 0; i < selectedObjects.Length; i++)
+            {
+                selectedObjects[i].SetActive(false);
+                stageViews[i].SetActive(false);
+            }
+            stageViews[beSelectedNum].SetActive(true);
+            selectedObjects[beSelectedNum].SetActive(true);
+        }
+
+        public void GotoStage()
         {
             for (int i = 0; i < buttonObjects.Length; i++)
             {
-                var button = buttonObjects[i].GetComponent<Button>();
-
-                button.enabled = false;
+                buttonObjects[i].enabled = false;
             }
-
             StartCoroutine(LoadSceneCoroutine(SceneName.PlayScene));
-            Stage.StageSceneManager.SetLoadStageIndex(stageNumber);
+            Stage.StageSceneManager.SetLoadStageIndex(beSelectedNum);
         }
 
         public void OnReturnButton()
@@ -42,6 +69,7 @@ namespace Cooking.SelectStage
 
         IEnumerator LoadSceneCoroutine(SceneName sceneName)
         {
+            Debug.Log(sceneName);
             if(sceneName == SceneName.PlayScene)
             {
                 Fader.FadeInAndOut(0.8f, 1.4f, 0.8f);
