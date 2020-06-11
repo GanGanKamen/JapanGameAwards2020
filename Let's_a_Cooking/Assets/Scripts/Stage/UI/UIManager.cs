beeaa37a99b2122224652cc1495c9c136a10f83a
GanGanKamen <m01171408c@edu.teu.ac.jp>
2020-06-11 15:04:31 +0900

    パワーメーター表記不具合を修正

--- Let's_a_Cooking/Assets/Scripts/Stage/UI/UIManager.cs
+++ Let's_a_Cooking/Assets/Scripts/Stage/UI/UIManager.cs
@@ -174,7 +174,7 @@ namespace Cooking.Stage
                                         //順番を決める数値を決定
                                         //var orderPower = _turnManager.PlayerDecideOrderValue(_turnManager.ActivePlayerIndex, _orderGage.value);
                                         var orderPower = _turnManager.PlayerDecideOrderValue(_turnManager.ActivePlayerIndex, _powerMeterValue);
-                                        var orderPowerText = (int)(orderPower / 10) + 1;
+                                        var orderPowerText = Mathf.FloorToInt(orderPower / 10) + 1;
                                         if (orderPowerText > 10) orderPowerText = 10;
                                         else if (orderPowerText < 1) orderPowerText = 1;
                                         _orderPowerTexts[_turnManager.ActivePlayerIndex].text = orderPowerText.ToString();
@@ -188,7 +188,7 @@ namespace Cooking.Stage
                                         _powerMeterValue = 100;
                                         _orderGagesOfInteger.sprite = ChoosePowerMeterUIOfInteger(_powerMeterValue / _orderMax);
                                         var orderPower = _turnManager.PlayerDecideOrderValue(_turnManager.ActivePlayerIndex, _powerMeterValue);
-                                        var orderPowerText = (int)(orderPower / 10) + 1;
+                                        var orderPowerText = Mathf.FloorToInt(orderPower / 10) + 1;
                                         if (orderPowerText > 10) orderPowerText = 10;
                                         else if (orderPowerText < 1) orderPowerText = 1;
                                         _orderPowerTexts[_turnManager.ActivePlayerIndex].text = orderPowerText.ToString();
@@ -367,7 +367,7 @@ namespace Cooking.Stage
             _playerListOrderPower[playerNumber].SetActive(true);
             _isAIListOrderPower[playerNumber].SetActive(true);
             _turnManager.AIDecideOrderValue(playerNumber);
-            var orderPowerText = (int)(_turnManager.OrderPower[playerNumber] / 10) + 1;
+            var orderPowerText = Mathf.FloorToInt(_turnManager.OrderPower[playerNumber] / 10) + 1;
             if (orderPowerText > 10) orderPowerText = 10;
             else if (orderPowerText < 1) orderPowerText = 1;
             _orderPowerTexts[playerNumber].text = orderPowerText.ToString();
